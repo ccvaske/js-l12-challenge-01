1665b04e642ea218b3268b41e5a23f322968f56c
ccvaske <74743770+ccvaske@users.noreply.github.com>
2022-09-07 21:16:37 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,4 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getImage = async function () {
-  const res = await fetch("https://picsum.photos/v2/list?limit=100");
-  const images = await res.json();
-  console.log(images);
-};
 
-getImage();
